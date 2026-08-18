1a6f8a5a7637e481094b464b815ce8a793aa0f5b
htp1082 <hlathueprue31@gmail.com>
2026-08-18 20:27:13 +0300

    Add to brands component

---
+++ src/component/brands/Brands.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import amazone from '../../assets/brands/amazon.png'
+import casio from '../../assets/brands/casio.png'
+import moonstar from '../../assets/brands/moonstar.png'
+import randstad from '../../assets/brands/randstad.png'
+import star from '../../assets/brands/star.png'
+import starpeople from '../../assets/brands/start_people.png'
+
+const Brands = () => {
+    return (
+        <div className='py-15'>
+            <div className='text-center'>
+                <h2 className='text-[28px] font-extrabold text-secondary'>We've helped thousands of sales teams</h2>
+            </div>
+
+            <div className='flex items-center justify-center gap-10 mt-10'>
+                <img src={casio} alt="Casio" />
+                  <img src={amazone} alt="Casio" />
+                    <img src={moonstar} alt="Casio" />
+                      <img src={star} alt="Casio" />
+                        <img src={starpeople} alt="Casio" />
+                          
+            </div>
+            <div className='border-2 border-dotted border-secondary mt-20'>
+
+            </div>
+        </div>
+    );
+};
+
+export default Brands;
