adc2ad8f7eb6e5be2426fe103296037d98fb35f2
htp1082 <hlathueprue31@gmail.com>
2026-08-21 15:15:55 +0300

    Upgrade swiper slide autoplay system

--- src/component/brands/Brands.jsx
+++ src/component/brands/Brands.jsx
@@ -1,31 +1,51 @@
-import React from 'react';
-import amazone from '../../assets/brands/amazon.png'
-import casio from '../../assets/brands/casio.png'
-import moonstar from '../../assets/brands/moonstar.png'
-import randstad from '../../assets/brands/randstad.png'
-import star from '../../assets/brands/star.png'
-import starpeople from '../../assets/brands/start_people.png'
+import React from "react";
+import "swiper/css";
+import amazone from "../../assets/brands/amazon.png";
+import casio from "../../assets/brands/casio.png";
+import moonstar from "../../assets/brands/moonstar.png";
+import randstad from "../../assets/brands/randstad.png";
+import star from "../../assets/brands/star.png";
+import starpeople from "../../assets/brands/start_people.png";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import Swiper styles
+import "swiper/css";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+const bangLogos = [amazone, casio, moonstar, randstad, star, starpeople];
 
 const Brands = () => {
-    return (
-        <div className='py-15'>
-            <div className='text-center'>
-                <h2 className='text-[28px] font-extrabold text-secondary'>We've helped thousands of sales teams</h2>
-            </div>
+  return (
+    <div>
+        <h2 className="text-secondary font-extrabold text-[28px] text-center mt-10">We've helped thousands of sales teams</h2>
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={30}
+      autoplay={{
+        delay: 2000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Autoplay, Pagination, Navigation]}
+      className="mySwiper"
+    >       
+        {bangLogos.map((logo, index) => (
+          <SwiperSlide key={index} className=" p-10">
+            <img src={logo} alt="logo" />
+          </SwiperSlide>
+        ))}
+     
+    </Swiper>
 
-            <div className='flex flex-col md:flex-row items-center justify-center gap-10 mt-10'>
-                <img src={casio} alt="Casio" />
-                  <img src={amazone} alt="amazone" />
-                    <img src={moonstar} alt="moonstar" />
-                      <img src={star} alt="star" />
-                        <img src={starpeople} alt="start people" />
-                          
-            </div>
-            <div className='border-2 border-dotted border-secondary mt-20'>
-
-            </div>
-        </div>
-    );
+        <div className="border-b-2  border-dotted border-secondary my-10"></div>
+      </div>
+  );
 };
 
 export default Brands;
