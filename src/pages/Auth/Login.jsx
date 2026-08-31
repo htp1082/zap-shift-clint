8944127e3aff6d2d7e1d7e265eb033f97b3d6825
htp1082 <hlathueprue31@gmail.com>
2026-08-31 15:08:51 +0300

    Add to login page and implement  to sign in

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,12 +1,60 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
 
 const Login = () => {
- 
-    return (
-        <div>
-            <h2>Login</h2>
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const {signInUser} = useAuth()
+
+  const loginHandler = (data) => {
+    console.log(data);
+    signInUser(data.email,data.password)
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
+  };
+
+  return (
+    <div>
+      <div className="card-body bg-white">
+        <form onSubmit={handleSubmit(loginHandler)} className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            className="input w-full"
+            {...register("email", { required: true })}
+            placeholder="Email"
+          />
+
+          {errors.email?.type === "required" && (
+            <p className="text-red-500 font-bold">Email is required</p>
+          )}
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password", { required: true, minLength: 6 })}
+            className="input w-full"
+            placeholder="Password"
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-red-500 font-bold">Password is required</p>
+          )}
+           {errors.password?.type === "minLength" && (
+            <p className="text-red-500 font-bold">Password is required</p>
+          )}
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Auth/Registration.jsx
+++ src/pages/Auth/Registration.jsx
@@ -1,33 +1,64 @@
-import React from "react";
+import React, { use } from "react";
 import { useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
 
 const Registration = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+
+  const {signUpUser} = useAuth();
 
   const handleRegistration = (data) => {
     console.log(data);
+    signUpUser(data.email,data.password)
+    .then(data =>{
+      console.log(data)
+      reset()
+    })
+    .catch((eror)=>{
+      console.log(eror)
+    })
+    
   };
   return (
     <div>
-      <div className="card-body">
+      <div className="card-body bg-white">
         <form onSubmit={handleSubmit(handleRegistration)} className="fieldset">
           <label className="label">Email</label>
           <input
             type="email"
-            className="input"
-            {...register("email",{required:true})}
+            className="input w-full"
+            {...register("email", { required: true })}
+            aria-invalid={errors.email ? "true" : "false"}
             placeholder="Email"
           />
+
+          {errors.email?.type === "required" && (
+            <p className="text-red-500 font-bold">Email is required</p>
+          )}
+
           <label className="label">Password</label>
           <input
             type="password"
-            className="input"
-            {...register("password",{required:true})}
+            className="input w-full"
+            {...register("password", {
+              required: true,
+              minLength: 6,
+            })}
+            aria-invalid={errors.password ? "true" : "false"}
             placeholder="Password"
           />
-          <div>
-            <a className="link link-hover">Forgot password?</a>
-          </div>
+          {errors.password?.type === "required" && (
+            <p className="text-red-500 font-bold">Passwordd is required</p>
+          )}
+
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500 font-bold">Passwordd must have 6 charecter or longer</p>
+          )}
           <button className="btn btn-neutral mt-4">Login</button>
         </form>
       </div>
